90ff4942e9d1cb719ab74179d615c443abc1be3d
23108 <2310874577@qq.com>
2019-08-31 08:53:07 +0800

    findorders修改

--- src/main/java/com/kzq/advance/service/impl/InformationServiceImpl.java
+++ src/main/java/com/kzq/advance/service/impl/InformationServiceImpl.java
@@ -17,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
